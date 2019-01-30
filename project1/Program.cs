b47611dd2ae399a4a4879c2260d9aa2b12552e83
eglandj <eglandj@dunwoody.edu>
2019-01-30 15:11:48 -0600

    Debugged a lot of issues, close to rubic

--- project1/Program.cs
+++ project1/Program.cs
@@ -11,13 +11,22 @@ namespace project1
         {
             player[,] playerProfile = JsonConvert.DeserializeObject<player[,]>(File.ReadAllText(@"C:\Users\eglandj\Documents\Visual Studio 2017\Projects\Project 1\project1\player.JSON"));
 
-            string[,] playerName = new string[playerProfile.GetLength(0), playerProfile.GetLength(1)];
-            for(int i = 0; i < playerProfile.GetLength(0); i++)
+            bool[,] pickedPlayer = new bool[playerProfile.GetLength(0), playerProfile.GetLength(1)];
+            for (int i = 0; i < playerProfile.GetLength(0); i++)
             {
                 for (int j = 0; j < playerProfile.GetLength(1); j++)
                 {
-                    playerName[i,j] = playerProfile[i, j].Name;
-                    
+                    pickedPlayer[i, j] = playerProfile[i, j].pickedPlayer;
+
+                }
+            }
+            string[,] playerName = new string[playerProfile.GetLength(0), playerProfile.GetLength(1)];
+            for (int i = 0; i < playerProfile.GetLength(0); i++)
+            {
+                for (int j = 0; j < playerProfile.GetLength(1); j++)
+                {
+                    playerName[i, j] = playerProfile[i, j].Name;
+
                 }
             }
             double[,] salary = new double[playerProfile.GetLength(0), playerProfile.GetLength(1)];
@@ -26,7 +35,7 @@ namespace project1
                 for (int j = 0; j < playerProfile.GetLength(1); j++)
                 {
                     salary[i, j] = playerProfile[i, j].Salary;
-                    
+
                 }
             }
             string[,] school = new string[playerProfile.GetLength(0), playerProfile.GetLength(1)];
@@ -35,7 +44,7 @@ namespace project1
                 for (int j = 0; j < playerProfile.GetLength(1); j++)
                 {
                     school[i, j] = playerProfile[i, j].School;
-                    
+
                 }
             }
             string[] position = new string[playerProfile.GetLength(0)];
@@ -62,15 +71,17 @@ namespace project1
             double moneyBank = 95000000;
             int row, column;
             int pickCount = 1;
+            bool effectiveDraft = false;
+            string lowCost = "You have made some COST EFFECTIVE draft choices.\n";
 
-            greeting();
+            greeting(moneyBank);
             keyCapture(out sentinel, pickCount);
 
             while (sentinel != ConsoleKey.X)
             {
                 Console.Clear();
 
-                outputTable(playerName, salary, school, position, rank);
+                outputTable(playerName, salary, school, position, rank, pickedPlayer);
 
                 row = getRow(position);
 
@@ -80,20 +91,20 @@ namespace project1
 
                 checkColumn(ref column);
 
-                accumPrice(playerName, salary, ref moneyBank, row, column);
+                accumPrice(playerName, school, salary, ref moneyBank, row, column, pickedPlayer);
 
-                costEffective(rankPick, ref pickCount, ref moneyBank);
+                costEffective(rankPick, ref pickCount, ref moneyBank, ref effectiveDraft, lowCost);
 
                 keyCapture(out sentinel, pickCount);
             }
 
-            outputPrice(moneyBank);
+            outputPrice(moneyBank, ref effectiveDraft, lowCost, pickedPlayer, rank, playerName, school, salary, position);
 
-        }
+        } //End of main
 
-        static void greeting()
+        static void greeting(double money)
         {
-            Console.WriteLine("Welcome to the 2019 NFL Draft!");
+            Console.WriteLine($"Welcome to the 2019 NFL Draft!\nYou will begin the draft with {money.ToString("c")}\n");
         }
 
         static void keyCapture(out ConsoleKey key, int pickCount)
@@ -102,15 +113,19 @@ namespace project1
             key = Console.ReadKey(true).Key;
             if (pickCount > 5)
             {
-                Console.WriteLine("You are at your max amount of picks.\nThe draft will now end.");
+                Console.Clear();
+                Console.WriteLine("You are at your max amount of picks.\nThe draft will now end. Please press any key to continue.");
+                Console.ReadKey();
+                key = ConsoleKey.X;
+
             }
         }
-        static void outputTable(string[,] name, double[,] sal, string[,] school, string[] pos, string[,] rank)
+        static void outputTable(string[,] name, double[,] sal, string[,] school, string[] pos, string[,] rank, bool[,] picked)
         {
             Console.Write("Position".PadRight(20));
             for (int i = 0; i < rank.GetLength(1); i++)
             {
-                Console.Write($"{rank[i,i]}".PadRight(20));
+                Console.Write($"{rank[i, i]}".PadRight(20));
             }
             Console.WriteLine("\n");
 
@@ -120,20 +135,48 @@ namespace project1
                 Console.Write($"{pos[i].PadRight(20)}");
                 for (var x = 0; x < name.GetLength(1); x++)
                 {
-                    Console.Write($"{name[i, x].PadRight(20)}");
+                    if (picked[i, x] == false)
+                    {
+                        Console.Write($"{name[i, x].PadRight(20)}");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Write($"{name[i, x].PadRight(20)}");
+                        Console.ResetColor();
+                    }
+
                 }
                 Console.WriteLine("");
                 Console.Write("".PadRight(20));
                 for (var x = 0; x < school.GetLength(1); x++)
                 {
-                    Console.Write($"{school[i, x].PadRight(20)}");
+                    if (picked[i, x] == false)
+                    {
+                        Console.Write($"{school[i, x].PadRight(20)}");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Write($"{school[i, x].PadRight(20)}");
+                        Console.ResetColor();
+                    }
                 }
                 Console.WriteLine("");
                 Console.Write("".PadRight(20));
                 for (var x = 0; x < sal.GetLength(1); x++)
                 {
 
-                    Console.Write($"{sal[i, x].ToString("c").PadRight(20)}");
+                    if (picked[i, x] == false)
+                    {
+                        Console.Write($"{sal[i, x].ToString("c").PadRight(20)}");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Write($"{sal[i, x].ToString("c").PadRight(20)}");
+                        Console.ResetColor();
+                    }
                 }
 
                 Console.WriteLine("");
@@ -143,83 +186,172 @@ namespace project1
         }
         static int getRow(string[] pos)
         {
-            int row; //Local
-            Console.WriteLine("Please select the position of the player you would like to draft\n");
+            int row;
+            Console.WriteLine("Please select the position of the player you would like to draft");
             for (int i = 0; i < pos.Length; i++)
             {
                 Console.WriteLine($"{i + 1}.) {pos[i]}");
             }
-            
+
             row = Convert.ToInt32(Console.ReadLine());
             return row = row - 1;
         }
         static int getColumn(List<int> rankPick, string[,] rank)
         {
-            int column; //Local
+            int column;
             Console.WriteLine("Please enter the rank of the player you would like to draft");
             for (int i = 0; i < rank.GetLength(1); i++)
             {
-                Console.WriteLine($"{i + 1}.) {rank[i,i]}");
+                Console.WriteLine($"{i + 1}.) {rank[i, i]}");
             }
             column = Convert.ToInt32(Console.ReadLine());
-            if (column < 4)
-            {
-                rankPick.Add(column);
-            }
+            rankPick.Add(column);
             return column = column - 1;
         }
         static void checkRow(ref int num)
         {
             while ((num < 0) || (num > 7))
             {
-                Console.WriteLine("Invalid entry, please enter a number between 1 and 8");
-                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Invalid entry, please enter a number between 1 and 8.");
+                num = Convert.ToInt32(Console.ReadLine()) - 1;
             }
         }
         static void checkColumn(ref int num)
         {
             while ((num < 0) || (num > 4))
             {
-                Console.WriteLine("Invalid entry, please enter a number between 1 and 5");
-                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Invalid entry, please enter a number between 1 and 5.");
+                num = Convert.ToInt32(Console.ReadLine()) - 1;
             }
         }
-        static void accumPrice(string[,] prod, double[,] price, ref double accum, int row, int column)
+        static void accumPrice(string[,] name, string[,] school, double[,] price, ref double accum, int row, int column, bool[,] picked)
         {
-            if (accum >= price[row, column])
+            if (picked[row, column] == false)
             {
-                accum -= price[row, column];
-                Console.WriteLine($"\nYou have selected {prod[row, column]} for {price[row, column].ToString("c")}");
-                Console.WriteLine($"You have {accum.ToString("c")} remaining.\n");
+                if (accum >= price[row, column])
+                {
+                    Console.Clear();
+                    accum -= price[row, column];
+                    Console.WriteLine($"You have selected {name[row, column]} from {school[row, column]} for {price[row, column].ToString("c")}");
+                    Console.WriteLine($"You have {accum.ToString("c")} remaining.\n");
+                    picked[row, column] = true;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid option");
+                    Console.WriteLine($"You only have {accum.ToString("c")} remaining. Please select a valid option.");
+                    return;
+                }
             }
             else
             {
-                Console.WriteLine("Invalid option");
-                Console.WriteLine($"You only have {accum.ToString("c")} remaining. Please select a valid option.");
+                Console.WriteLine("This player has already been picked.");
                 return;
             }
         }
-        static void outputPrice(double totalPrice)
+
+        static void costEffective(List<int> rankPick, ref int pick, ref double accum, ref bool effectiveDraft, string lowCost)
         {
-            Console.Clear();
-            Console.WriteLine("Based on your selections, you have {0} remaining", totalPrice.ToString("c"));
-        }
-        static void costEffective(List<int> rankPick, ref int pick, ref double accum)
-        {
-            for (int i = 0; i < rankPick.Count; i++)
+            if (pick == 3)
             {
-                if (rankPick[i] < 3)
+                for (int i = 0; i < rankPick.Count; i++)
                 {
-                    if (pick > 2 && accum > 30000000)
+                    rankPick.Sort();
+                    rankPick.Reverse();
+
+                    if (rankPick[i] > 3)
                     {
-                        Console.WriteLine("You have made some COST EFFECTIVE draft choices\n");
                         break;
+                    }
+                    else
+                    {
+                        if (accum > 30000000)
+                        {
+                            Console.WriteLine(lowCost);
+                            effectiveDraft = true;
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
                 }
             }
             pick++;
         }
-        
+        static void outputPrice(double totalPrice, ref bool effectiveDraft, string lowCost, bool[,] picked, string[,] rank, string[,] name, string[,] school, double[,] price, string[] position)
+        {
+            Console.Clear();
+            if (effectiveDraft == true)
+            {
+                Console.WriteLine($"Congratulations, {lowCost}");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations on completing your draft!");
+            }
+            Console.WriteLine("Based on your selections, you have {0} remaining.", totalPrice.ToString("c"));
+            Console.WriteLine("You have drafted:\n");
+
+            for (var i = 0; i < name.GetLength(0); i++)
+            {
+                for (var x = 0; x < name.GetLength(1); x++)
+                {
+                    if (picked[i, x] == true)
+                    {
+                        Console.Write($"{rank[i, x].PadRight(20)}");
+                    }
+                }
+            }
+            Console.WriteLine("");
+            for (var i = 0; i < name.GetLength(0); i++)
+            {
+                for (var x = 0; x < name.GetLength(1); x++)
+                {
+                    if (picked[i, x] == true)
+                    {
+                        Console.Write($"{position[i].PadRight(20)}");
+                    }
+                }
+            }
+            Console.WriteLine("");
+            for (var i = 0; i < name.GetLength(0); i++)
+            {
+                for (var x = 0; x < name.GetLength(1); x++)
+                {
+                    if (picked[i, x] == true)
+                    {
+                        Console.Write($"{name[i, x].PadRight(20)}");
+                    }
+                }
+            }
+            Console.WriteLine("");
+            for (var i = 0; i < school.GetLength(0); i++)
+            {
+                for (var x = 0; x < school.GetLength(1); x++)
+                {
+                    if (picked[i, x] == true)
+                    {
+                        Console.Write($"{school[i, x].PadRight(20)}");
+                    }
+                }
+            }
+            Console.WriteLine("");
+            for (var i = 0; i < price.GetLength(0); i++)
+            {
+                for (var x = 0; x < price.GetLength(1); x++)
+                {
+                    if (picked[i, x] == true)
+                    {
+                        Console.Write($"{price[i, x].ToString("c").PadRight(20)}");
+                    }
+                }
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("This application will now close.");
+        }
     }
     class player
     {
@@ -228,6 +360,12 @@ namespace project1
         public string Name { get; set; }
         public string School { get; set; }
         public double Salary { get; set; }
-        
+        public bool pickedPlayer { get; set; }
+
+        public player()
+        {
+            pickedPlayer = false;
+        }
+
     }
 }
