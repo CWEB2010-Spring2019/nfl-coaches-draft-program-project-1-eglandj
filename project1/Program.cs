6d42d3ff612635c360da4dbc0b3c47c70be3295f
eglandj <eglandj@dunwoody.edu>
2019-01-30 21:17:53 -0600

    Idk

--- project1/Program.cs
+++ project1/Program.cs
@@ -10,7 +10,7 @@ namespace project1
         static void Main(string[] args)
         {
             //Extracting data from a JSON File
-            player[,] playerProfile = JsonConvert.DeserializeObject<player[,]>(File.ReadAllText(@"C:\Users\eglandj\Documents\Visual Studio 2017\Projects\Project 1\player.JSON"));
+            player[,] playerProfile = JsonConvert.DeserializeObject<player[,]>(File.ReadAllText(@"C:\Users\eglandj\Documents\Visual Studio 2017\Projects\Project 1\project1\player.JSON"));
 
             //Creating arrays for the extracted data
             bool[,] pickedPlayer = new bool[playerProfile.GetLength(0), playerProfile.GetLength(1)];
